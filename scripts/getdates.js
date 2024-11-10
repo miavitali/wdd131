ef5e2b88dcd051397100f8fabe8fbe93e096d909
miavitali <vitalimdlaunq@gmail.com>
2024-11-09 23:41:14 -0300

    java

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 const currentYear = document.querySelector("#currentYear")
 const today = new Date();
-currentyearyear.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
+currentYear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 const lastModified = document.querySelector("#lastModified")
 lastModified.innerHTML = `Last modified: ${document.lastModified}`
