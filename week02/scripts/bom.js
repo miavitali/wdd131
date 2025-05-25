0b28c7a17bf1f2de89a28f6b2d2ea6286c87eb6f
miavitali <vitalimdlaunq@gmail.com>
2025-05-24 22:47:25 -0300

    week3

---
+++ week02/scripts/bom.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("");
+
+const li = document.createElement("li");
+const deleteButton = document.createElement("button");
